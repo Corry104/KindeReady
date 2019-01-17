e28c03cd331541092b6c8cc47e66ac92ee9f0550
Thanyah Kung <pitchayarasm.kunghae@hotmail.com>
2019-01-16 22:26:13 -0800

    act1 shapes

--- ASSETS/JS/ACTIVITIES/SHAPESNCOLORS/shapesAct1.js
+++ ASSETS/JS/ACTIVITIES/SHAPESNCOLORS/shapesAct1.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+    $("#changeStudent").on("click",function(event) {
+        
+    });
 
+    $("#logout").on("click",function(event) {
+
+    });
+
+    $("#previous").on("click",function(event) {
+
+    });
+
+    $("#next").on("click",function(event) {
+
+    });
 });
